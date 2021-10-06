5dfdcedb56af45470181d7f9e9cf686d9e5d8142
Pedro Delgado <pedro.delgado2017@mesacharter.org>
2021-10-06 14:53:42 +0000

    completed problem 1

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 //1
-function absoluteValue(){
-  
+//RETURN THE ABSOLUTE VALUE OF A NUMBER
+//IF THE VALUE IS NOT A NUMBER RETURN 'NULL'
+function absoluteValue(num){
+    if(typeof num === "number") {
+        if(num < 0){
+            return (num * -1);
+        }else{
+            return num
+        }
+    } else {
+        return null
+    }
 }
+console.log(absoluteValue("car"))
 
-
-
-
+//2
